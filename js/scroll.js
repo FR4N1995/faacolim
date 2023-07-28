49665b7a516bafe4519baa035fbb49add1d83a98
FR4N1995 <fran7paco10@gmail.com>
2023-07-28 12:06:30 -0600

    se agrego portada en la pestalla catalogo y se comento una linea de codigo en el scroll

--- js/scroll.js
+++ js/scroll.js
@@ -11,7 +11,7 @@
         // const catalogopage = document.querySelector('.catalogo__tabs');
         const body = document.querySelector('body');
         window.addEventListener('scroll', function(){
-             console.log(scrollFijo.getBoundingClientRect())
+            //  console.log(scrollFijo.getBoundingClientRect())
             // console.log(catalogopage.getBoundingClientRect())
             if (scrollFijo.getBoundingClientRect().top < 0 ) {
                 nav.classList.add('fijo');
